484529d2228b7fb1e32d43390428f3404b1637e1
CODERAKS11 <kk9546x@gmail.com>
2026-07-15 13:45:42 +0530

    integrated frontend

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
