d06066d5c52e20d9153795cea6707327febda28b
CODERAKS11 <kk9546x@gmail.com>
2026-07-15 10:47:01 +0530

    department name validators added

--- department/serializers.py
+++ department/serializers.py
@@ -5,3 +5,9 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
+
+    def validate_department_name(self, value):
+        value = value.strip()
+        if len(value) < 2:
+            raise serializers.ValidationError("Department name is too short.")
+        return value
